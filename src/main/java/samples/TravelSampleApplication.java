582b597eec31b8160fabe08c5d0c6f4aeda51a13
Michael Nitschinger <michael@nitschinger.at>
2015-08-24 10:34:23 +0200

    Initial Project Stub

---
+++ src/main/java/samples/TravelSampleApplication.java
@@ -0,0 +1,12 @@
+package samples;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelSampleApplication.class, args);
+    }
+}
